a56d42e8835f8b16932689fef698de1b35065029
owen-kellie-smith <owen.kelliesmith@gmail.com>
2015-10-07 07:02:58 +0100

    Test for making xml from request (currently fils)

--- FinancialMathematics/phpunit-tests/ct1_form_Test.php
+++ FinancialMathematics/phpunit-tests/ct1_form_Test.php
@@ -38,6 +38,16 @@ class CT1_Concept_Test extends PHPUnit_Framework_TestCase
 		$c = $x->get_controller( array( 'request'=>'get_interest','m'=>1,'advance'=>1,'i_effective'=>0.1) );
 	  $this->assertTrue( isset($c['formulae']) ) ;
 	  $this->assertTrue( isset($c['xml-form']) ) ;
+	  $this->assertTrue( isset($c['xml-form']['form']) ) ;
+  }  
+
+  public function test_full_input_XML()
+  {
+	  $x = new CT1_Form_XML();
+		$x->set_text( array( 'request'=>'get_interest','m'=>1,'advance'=>1,'i_effective'=>0.1) );
+		$c = $x->get_calculator( array());
+		$expected="<parameters><request>get_interest</request><m>1</m><advance>1</advance><i_effective>0.1</i_effective></parameters>";
+		$this->assertEquals( $c['values']['xml'],$expected ) ;
   }  
 
 }
