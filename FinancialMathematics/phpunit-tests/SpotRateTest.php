7db75498938beeb4e9c43b1ae6ad9d946c2416b3
owen-kellie-smith <owen.kelliesmith@gmail.com>
2015-10-22 23:24:28 +0100

    Revert "try to track travis error in spot rates"
    This reverts commit 830ceb248a4f985a670a6bef0851c9a768144f3a.

--- FinancialMathematics/phpunit-tests/SpotRateTest.php
+++ FinancialMathematics/phpunit-tests/SpotRateTest.php
@@ -60,7 +60,6 @@ class CT1_Spot_Rate_Test extends PHPUnit_Framework_TestCase
                 )
 
         )));
-	  $this->assertEquals( '', $c['warning']);
 	  $this->assertTrue( isset($c['form']) ) ;
 	  $this->assertTrue( isset($c['table']) ) ;
 	  $this->assertTrue( isset($c['output']) ) ;
@@ -86,7 +85,6 @@ class CT1_Spot_Rate_Test extends PHPUnit_Framework_TestCase
                 )
 
         )));
-	  $this->assertEquals( '', $c['warning']);
 	  $this->assertTrue( isset($c['formulae']) ) ;
 	  $this->assertTrue( isset($c['form']) ) ;
 	  $this->assertTrue( isset($c['output']) ) ;
